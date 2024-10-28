dc3d0fc9d7b9cb96ca13a7edd9f1b106e130c054
Fahad1656 <mohammad.rejwanul.islam@g.bracu.ac.bd>
2024-10-28 15:57:25 +0600

    updated again

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import uvicorn
 from routes.muzzle import router as muzzle_router
 from routes.disease import router as disease_router
 from routes.detect import router as detect_router
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 # Initialize FastAPI app
 app = FastAPI()
