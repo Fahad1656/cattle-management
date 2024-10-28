a90e4792675021f371f41654b31bf1fc198a5b20
Fahad1656 <mohammad.rejwanul.islam@g.bracu.ac.bd>
2024-10-28 16:12:42 +0600

    updated

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
 import os
 from fastapi import FastAPI
 import uvicorn
 from routes.muzzle import router as muzzle_router
-from routes.disease import router as disease_router
+
 from routes.detect import router as detect_router
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
@@ -13,12 +13,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 app = FastAPI()
 
 app.include_router(muzzle_router, prefix="/api", tags=["Muzzle Detection"])
-app.include_router(disease_router, prefix="/api", tags=["Disease Detection"])
+
 app.include_router(detect_router, prefix="/api", tags=["Disease Detection part2"])
+
 
 def run_app():
     # Start the FastAPI app with Uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 9000)))  # Use PORT from environment variable
+    uvicorn.run(
+        app, host="0.0.0.0", port=int(os.environ.get("PORT", 9000))
+    )  # Use PORT from environment variable
+
 
 if __name__ == "__main__":
     run_app()
